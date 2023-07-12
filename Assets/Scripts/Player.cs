0969761f25b18276fff18ce690d41de7b454700f
monil1301 <mvshah1301@gmail.com>
2023-07-13 01:11:21 +0530

    Added moving the player with Legacy input.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   // Serialized fields
+   [SerializeField] private float moveSpeed = 7;
+   
+   // Unity Methods
+   private void Update() 
+   {
+      Vector2 inputVector = new Vector2(0, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+      // Move Left
+      if (Input.GetKey(KeyCode.W))
+      {
+         inputVector.y = 1;
+      }
+
+      // Move Right
+      if (Input.GetKey(KeyCode.S))
+      {
+         inputVector.y = -1;
+      }
+
+      // Move Up
+      if (Input.GetKey(KeyCode.A))
+      {
+         inputVector.x = -1;
+      }
+
+      // Move Down
+      if (Input.GetKey(KeyCode.D))
+      {
+         inputVector.x = 1;
+      }
+
+      inputVector.Normalize();
+      Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+      transform.position += (moveDirection * moveSpeed * Time.deltaTime);
+   }
+
+   // Private Methods
+
+   // Public Methods
 }
