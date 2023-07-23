c727f5f93fd86eefa36df94f9ceaba7c1d9faef7
monil1301 <mvshah1301@gmail.com>
2023-07-24 01:33:50 +0530

    Added cutting of kitchen objects and its recipe

--- Assets/Scripts/Counters/CuttingCounter.cs
+++ Assets/Scripts/Counters/CuttingCounter.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 { 
+    // Serialized fields
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
+
+    // Private Methods
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        // Get processed kitchen object of a kitchen object. Eg: Get tomoto slices from tomato
+        foreach (var cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return cuttingRecipeSO.output;
+            }
+        }
+
+        return null;
+    }
+
+    private bool HasRecipeForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        // Check if the kitchen object can be processed ot not. Eg: Tomato to tomoto slices
+        foreach (var cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     // Public methods
     public override void Interact(Player player)
     {
@@ -13,8 +45,12 @@ public class CuttingCounter : BaseCounter
             // Counter does not have anything, so check if player has kitchen object to place on the counter
             if (player.HasKitchenObject())
             {   
-                // Place the kitchen object from player on the counter
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                // Only place the object if it can be processed
+                if (HasRecipeForInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    // Place the kitchen object from player on the counter
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
             }
         }
         else
@@ -30,11 +66,15 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternate(Player player)
     {
-        // Check if the counter has kitchen object or not
-        if (HasKitchenObject())
+        // Check if the counter has kitchen object and only interact if it can be processed
+        if (HasKitchenObject() && HasRecipeForInput(GetKitchenObject().GetKitchenObjectSO()))
         {
-            // Counter has a kitchen object, so destory it and spawn the sliced or cooked kitchen object
+            // Get the processed kitchen object for a kitchen object
+            KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+
+            // Destory kitchen object and spawn the sliced kitchen object
             GetKitchenObject().DestroySelf();
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
         }
     }
 }
