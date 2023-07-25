11dc1fc806e6d34c888c5f3867210e31f1acd4f5
monil1301 <mvshah1301@gmail.com>
2023-07-26 01:36:27 +0530

    Added ui elements looking at camera

---
+++ Assets/Scripts/Camera/LookAtCamera.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+
+    // Serialized field
+    [SerializeField] private Mode mode;
+
+    // Unity Methods
+    private void LateUpdate() 
+    {
+        switch (mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 directionFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt (transform.position + directionFromCamera);
+                break;
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
+    }
+}
