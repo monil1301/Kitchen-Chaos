27e16e29ab7c5167758aaf8d333021c4f36a8387
monil1301 <mvshah1301@gmail.com>
2023-08-21 01:54:23 +0530

    Added footsteps sound for the player moving

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -8,8 +8,16 @@ public class SoundManager : MonoBehaviour
     // Serialized fields
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    // Public fields
+    public static SoundManager Instance { get; private set; }
+
     // Unity Methods
-    private void Start() 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
     {
         DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
         DeliveryManager.Instance.OnRecipeFailure += DeliveryManager_OnRecipeFailure;
@@ -22,11 +30,12 @@ public class SoundManager : MonoBehaviour
     }
 
     // Private Methods
-    private void PlaySound (AudioClip[] audioClipArray, Vector3 position, float volume = 1f) {
-        PlaySound(audioClipArray[UnityEngine.Random. Range (0, audioClipArray.Length)], position, volume);
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
+    {
+        PlaySound(audioClipArray[UnityEngine.Random.Range(0, audioClipArray.Length)], position, volume);
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f) 
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
     {
         AudioSource.PlayClipAtPoint(audioClip, position, volume);
     }
@@ -36,29 +45,39 @@ public class SoundManager : MonoBehaviour
         DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
         PlaySound(audioClipRefsSO.deliverySuccess, deliveryCounter.transform.position);
     }
-    
+
     private void DeliveryManager_OnRecipeFailure(object sender, EventArgs eventArgs)
     {
         DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
         PlaySound(audioClipRefsSO.deliveryFail, deliveryCounter.transform.position);
-    }   
+    }
 
-    private void CuttingCounter_OnAnyCut(object sender, EventArgs e) {
+    private void CuttingCounter_OnAnyCut(object sender, EventArgs e)
+    {
         CuttingCounter cuttingCounter = sender as CuttingCounter;
         PlaySound(audioClipRefsSO.chop, cuttingCounter.transform.position);
     }
-    
-    private void BaseCounter_OnAnyObjectPlacedHere(object sender, EventArgs e) {
+
+    private void BaseCounter_OnAnyObjectPlacedHere(object sender, EventArgs e)
+    {
         BaseCounter baseCounter = sender as BaseCounter;
         PlaySound(audioClipRefsSO.objectDrop, baseCounter.transform.position);
     }
-    
-    private void TrashCounter_OnAnyObjectTrashed(object sender, EventArgs e) {
+
+    private void TrashCounter_OnAnyObjectTrashed(object sender, EventArgs e)
+    {
         TrashCounter trashCounter = sender as TrashCounter;
         PlaySound(audioClipRefsSO.objectDrop, trashCounter.transform.position);
     }
 
-    private void Player_OnPickedSomething(object sender, EventArgs e) {
+    private void Player_OnPickedSomething(object sender, EventArgs e)
+    {
         PlaySound(audioClipRefsSO.objectPickup, Player.Instance.transform.position);
+    }
+
+    // Public Methods
+    public void PlayFootstepsSound(Vector3 position, float volume)
+    {
+        PlaySound(audioClipRefsSO.footstep, position, volume);
     }
 }
